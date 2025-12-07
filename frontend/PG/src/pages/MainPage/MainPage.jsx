e9058f60c656a14d5899bd90a80c393f5cabcc05
yunyi <hong.yy041@gmail.com>
2025-12-07 20:12:28 +0900

    Fix: Details 코드 수정

--- frontend/PG/src/pages/MainPage/MainPage.jsx
+++ frontend/PG/src/pages/MainPage/MainPage.jsx
@@ -1,26 +1,9 @@
-// src/pages/MainPage/MainPage.jsx
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import Details from "./Details/Details"
 
-export default function MainPage() {
-  const navigate = useNavigate();
-
-  const goToDetails = () => {
-    navigate("/details");  
-  };
-
-  return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-
-      <h1 className="text-2xl font-bold mb-6">메인 페이지</h1>
-
-      <button
-        onClick={goToDetails}
-        className="px-6 py-3 bg-blue-600 text-white rounded-xl shadow hover:bg-blue-700 transition"
-      >
-        상세 페이지로 이동하기
-      </button>
-
+export default function MainPage(){
+  return(
+    <div>
+      <Details />
     </div>
   );
 }
